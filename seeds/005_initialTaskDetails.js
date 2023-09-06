f4200b22c0825ca2df3d73f23bbd887b4b40ec25
Richard Ly <39727690+richardly9319@users.noreply.github.com>
2023-09-05 21:24:20 -0700

    changed some initial seed data

--- seeds/005_initialTaskDetails.js
+++ seeds/005_initialTaskDetails.js
@@ -6,8 +6,6 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('taskdetails').del()
   await knex('taskdetails').insert([
-    {id: 1, task_id: 1, user_id:2, detail_text: 'get atleast 10 mins of sunlight in eyes'},
-    {id: 2, task_id: 2, user_id:2, detail_text: 'at least 45secs at coldest setting'},
     {id: 3, task_id: 7, user_id:2, detail_text: 'chicken breast'},
     {id: 4, task_id: 7, user_id:2, detail_text: 'eggs'},
     {id: 5, task_id: 7, user_id:2, detail_text: 'kale'},
--- seeds/009_initialInspirations.js
+++ seeds/009_initialInspirations.js
@@ -6,7 +6,7 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('inspiration').del()
   await knex('inspiration').insert([
-    {id: 1, user_id: 2, item_name: `"I’ve failed over and over and over again in my life, and that is why I succeed"`},
-    {id: 2, user_id: 2, item_name: `“The best time to plant a tree was 20 years ago. The second best time is now.”`},
+    {id: 1, user_id: 2, item_name: `"I’ve failed over and over and over again in my life, and that is why I succeed" - Michael Jordan`},
+    {id: 2, user_id: 2, item_name: `“The best time to plant a tree was 20 years ago. The second best time is now.” - Chinese Proverb`},
   ]);
 };
