6b4c9231c2e02764a76594a4f662b28e6566f1de
Richard Ly <39727690+richardly9319@users.noreply.github.com>
2023-09-17 00:37:35 -0700

    taking in Google auth login from frontend

---
+++ routes/authRouter.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const { OAuth2Client } = require('google-auth-library');
+const jwt = require('jsonwebtoken');
+const knex = require("knex")(require("./knexfile.js"));
+
+
+const router = express.Router();
+
+const googleClientId = process.env.GOOGLE_CLIENT_ID;
+
+
+const client = new OAuth2Client(googleClientId);
+
+
+
+router.post('/google', async (req, res) => {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: req.body.token,
+            audience: googleClientId,
+        });
+
+        const payload = ticket.getPayload();
+        const googleUserId = payload['sub'];
+
+        // Check if the user exists in your database
+        let user = await knex('users').where({ googleUserId }).first();
+
+        // If not, create a new user
+        if (!user) {
+            await knex('users').insert({
+                googleUserId,
+                // ... any other fields you want to store
+            });
+            user = await knex('users').where({ googleUserId }).first();
+        }
+
+        // Create a JWT token
+        const userToken = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+        res.json({
+            user,
+            token: userToken
+        });
+
+    } catch (error) {
+        console.error("Error in /auth/google:", error);
+        res.status(500).json({ error: 'Authentication failed' });
+    }
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const userRoute = require("./routes/userRouter");
+const authRoute = require("./routes/authRouter");
+
 
 const knex = require("knex")(require("./knexfile.js"));
 
@@ -12,11 +14,15 @@ const PORT = process.env.PORT || 5050;
 // CORS middleware
 app.use(cors());
 
+
+
 // Body parser middleware
 app.use(express.json());
 
 // User routes
 app.use("/", userRoute);
+
+app.use("/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}`);
