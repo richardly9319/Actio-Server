ff2d145caac144cb556cf3e2aa3e67f49f7efe2e
Richard Ly <39727690+richardly9319@users.noreply.github.com>
2023-09-16 22:35:48 -0700

    fixing Cross-Origin-Opener-Policy policy would block the window.closed call. bug

--- server.js
+++ server.js
@@ -9,12 +9,21 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5050;
 
-
-
+// CORS middleware
 app.use(cors());
-app.use(express.json());
-app.use("/", userRoute);
 
+// Middleware to set the COOP headers
+app.use((req, res, next) => {
+  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp"); // It's good practice to set COEP alongside COOP
+  next();
+});
+
+// Body parser middleware
+app.use(express.json());
+
+// User routes
+app.use("/", userRoute);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${process.env.PORT}`);
