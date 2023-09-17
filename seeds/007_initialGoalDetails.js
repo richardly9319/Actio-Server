3baf95e9b6dd7ea6b647f3a24ffa5163f6feadd5
Richard Ly <39727690+richardly9319@users.noreply.github.com>
2023-09-17 15:52:10 -0700

    seed data

--- seeds/007_initialGoalDetails.js
+++ seeds/007_initialGoalDetails.js
@@ -11,5 +11,7 @@ exports.seed = async function(knex) {
     {id: 4, section_id: 4, user_id: 2, detail_text: 'full body weight training routine'},
     {id: 5, section_id: 4, user_id: 2, detail_text: 'HIIT cardio 2x a week'},
     {id: 7, section_id: 5, user_id: 2, detail_text: 'Practice 45mins 2x per week'},
+    {id: 8, section_id: 5, user_id: 2, detail_text: 'River Flows in You'},
+    {id: 8, section_id: 5, user_id: 2, detail_text: 'Kingdom Hearts Dearly Beloved'},
   ]);
 };
