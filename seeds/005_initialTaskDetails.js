13beb3359f890b1853ada1892fb292bf7877a4e1
Richard Ly <39727690+richardly9319@users.noreply.github.com>
2023-09-17 15:12:51 -0700

    a

--- seeds/005_initialTaskDetails.js
+++ seeds/005_initialTaskDetails.js
@@ -7,7 +7,7 @@ exports.seed = async function(knex) {
   await knex('taskdetails').insert([
     {id: 3, task_id: 7, user_id:2, detail_text: 'chicken breast'},
     {id: 5, task_id: 7, user_id:2, detail_text: 'kale'},
-    {id: 10, task_id: 7, user_id:2, detail_text: 'almond milk'},
+    {id: 2, task_id: 7, user_id:2, detail_text: 'almond milk'},
     {id: 6, task_id: 8, user_id:2, detail_text: 'buy PL adhesive product from Home Depot'},
     {id: 7, task_id: 10, user_id:2, detail_text: 'Full body training followed by 15min LISS'},
     {id: 8, task_id: 6, user_id:2, detail_text: 'When user clicks on circle icon to left of item'},
