d50377480bcd4e7e617303e034c0f6ce736cbe47
Richard Ly <39727690+richardly9319@users.noreply.github.com>
2023-09-16 23:26:42 -0700

    deleted COOP and COEP headers

--- server.js
+++ server.js
@@ -12,13 +12,6 @@ const PORT = process.env.PORT || 5050;
 // CORS middleware
 app.use(cors());
 
-// Middleware to set the COOP and COEP headers
-app.use((req, res, next) => {
-  res.header("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
-  res.header("Cross-Origin-Embedder-Policy", "require-corp"); // It's good practice to set COEP alongside COOP
-  next();
-});
-
 // Body parser middleware
 app.use(express.json());
 
